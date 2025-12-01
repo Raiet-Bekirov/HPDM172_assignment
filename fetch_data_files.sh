f5a0b7c98aad5ad04ed604b8a76fd7a84f375293
Becky Griffiths <reg222@exeter.ac.uk>
2025-12-01 13:42:31 +0000

    Modifty fetch_data_files.sh to use curl instead of wget for wider compatibility

--- fetch_data_files.sh
+++ fetch_data_files.sh
@@ -2,11 +2,11 @@
 
 # # Script to download input text files with data for SQL hospitals database.
 
-wget https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/hospitals_table.txt
-wget https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/diseases_table.txt
-wget https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/doctors_table.txt
-wget https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/patients_table.txt
-wget https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/medications_table.txt
-wget https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/prescriptions_table.txt
-wget https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/appointments_table.txt
-wget https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/lab_results_table.txt
+curl -o hospitals_table.txt https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/hospitals_table.txt
+curl -o diseases_table.txt https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/diseases_table.txt
+curl -o doctors_table.txt https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/doctors_table.txt
+curl -o patients_table.txt https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/patients_table.txt
+curl -o medications_table.txt https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/medications_table.txt
+curl -o prescriptions_table.txt https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/prescriptions_table.txt
+curl -o appointments_table.txt https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/appointments_table.txt
+curl -o lab_results_table.txt https://raw.githubusercontent.com/Raiet-Bekirov/HPDM172_assignment/refs/heads/main/data_generation/generated_data/lab_results_table.txt
