23e23240b335afd33fe3cacfb746b08a2b045fd6
Becky Griffiths <reg222@exeter.ac.uk>
2025-12-05 00:48:24 +0000

    Modify output ordering of get_pts_at_small_hosps.sql

--- sql_queries/get_pts_at_small_hosps.sql
+++ sql_queries/get_pts_at_small_hosps.sql
@@ -8,4 +8,6 @@ FROM patients p
 JOIN doctors d ON p.doctor_id = d.doctor_id
 JOIN hospitals h ON d.hospital_id = h.hospital_id
 WHERE h.no_of_beds < 400
-ORDER BY h.no_of_beds ASC;
+ORDER BY 
+	h.no_of_beds ASC,
+	p.patient_id ASC;
