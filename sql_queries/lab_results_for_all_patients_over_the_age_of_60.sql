ed9c3c2a586100b4074d4df6fdabb61a294c336f
Ata Ugur Ozturk <85750641+Atqss@users.noreply.github.com>
2025-12-07 22:59:02 +0000

    Update lab_results_for_all_patients_over_the_age_of_60.sql

--- sql_queries/lab_results_for_all_patients_over_the_age_of_60.sql
+++ sql_queries/lab_results_for_all_patients_over_the_age_of_60.sql
@@ -1,10 +1,11 @@
-SELECT 
-    ->     lr.lab_result_id,
-    ->     lr.test_date,
-    ->     lr.test_type,
-    ->     lr.test_result,
-    ->     p.patient_id
-    -> FROM lab_results lr
-    -> JOIN patients p ON lr.patient_id = p.patient_id
-    -> WHERE p.patient_dob < CURDATE() - INTERVAL 60 YEAR
-    -> ORDER BY lr.test_date DESC;
+SELECT
+    lr.lab_result_id,
+    lr.test_date,
+    lr.test_type,
+    lr.test_result,
+    p.patient_id
+FROM lab_results lr
+JOIN patients p ON lr.patient_id = p.patient_id
+WHERE p.patient_dob < CURDATE() - INTERVAL 60 YEAR
+ORDER BY lr.test_date DESC;
+
