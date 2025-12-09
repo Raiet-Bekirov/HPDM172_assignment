a256282531c6fa65fb32c98a99867a5fc244083a
Maitry-t <mt896@exeter.ac.uk>
2025-12-09 02:44:19 +0000

    Add SQL query to insert a new patient record

---
+++ sql_queries/add_new_patient.sql
@@ -0,0 +1,3 @@
+insert into patients (patient_id, patient_name, patient_dob, patient_address, doctor_id)
+values
+(601, 'Alice Brown','1998-12-12','17 Milverton court, Leamington spa CV32 5JA', '23');
