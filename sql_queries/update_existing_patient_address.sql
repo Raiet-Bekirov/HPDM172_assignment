e34baf39ffac3ae42442f6972fca34291be5fc1e
Becky Griffiths <reg222@exeter.ac.uk>
2025-12-10 23:54:05 +0000

    Make query accept user specified input

--- sql_queries/update_existing_patient_address.sql
+++ sql_queries/update_existing_patient_address.sql
@@ -1,9 +1,3 @@
-SET @Pt2UpdateID = 56
-SET @Address2Update = '221B Baker Street, London NW1 6XE'
-
 UPDATE patients
 SET patient_address= @Address2Update
 WHERE patient_id = @Pt2UpdateID;
-
-SELECT * FROM patients
-WHERE patient_id = 56;
